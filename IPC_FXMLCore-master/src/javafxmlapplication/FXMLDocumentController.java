b278a785c95f77db7c7af82f15c71a6cfde938bb
Marcos004 <marcosjaimesma@gmail.com>
2024-05-25 01:40:40 +0200

    backup

--- IPC_FXMLCore-master/src/javafxmlapplication/FXMLDocumentController.java
+++ IPC_FXMLCore-master/src/javafxmlapplication/FXMLDocumentController.java
@@ -1,187 +1,283 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javafxmlapplication;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.util.Duration;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.util.Callback;
+import model.Acount;
+import model.AcountDAOException;
 import model.Category;
+import model.Charge;
 
-/**
- *
- * @author jsoler
- */
-public class FXMLDocumentController implements Initializable {
-    //========================================================
-    // objects defined into FXML file with fx:id 
-    @FXML
-    private GridPane back;
 
-    @FXML
-    private ComboBox<?> comboBox;
+//grid.add((Node) loader.load(), width, height, row.spane, column.span)
 
-    @FXML
-    private AnchorPane leftMenu;
+public class FXMLDocumentController implements Initializable{
 
+    FXMLDocumentController controllerL;
+    Acount account;
     @FXML
-    private ListView<String> listV;
-
+    private Button adder;
+    
     @FXML
-    private Label menuButton1;
-
+    private GridPane grid;
+    
     @FXML
-    private Label orderB;
-
-    @FXML
-    private Text title;
-
-    @FXML
-    private Label menuButton2;
-
-    @FXML
-    private Label menuButton3;
+    private AnchorPane back;
     
     @FXML
     private ScrollPane scrollPane;
     
-    private ObservableList<String> listObjects;
-    /*
-        Cuestión, hago un objeto intermedio para facilitar la cosa y no hacer un cellFactory??
-    */
-    @FXML
-    private HBox whole;
-    private Stage stage;
+    private int counterObj;
     
-    @FXML
-    void enteredM(MouseEvent event)
-    {
-        menuButton1.setVisible(true);
-        menuButton2.setVisible(true);
-        menuButton3.setVisible(true);
-        
-        whole.setPrefWidth(stage.getWidth());
-        listV.setPrefWidth(stage.getWidth()*0.7);
-        back.setPrefWidth(stage.getWidth()*0.7);
-        scrollPane.setPrefWidth(stage.getWidth()*0.7);
-        leftMenu.setPrefWidth(stage.getWidth()*0.3);
-        
-    }
-    @FXML
-    void exitedM(MouseEvent event)
-    {
-        menuButton1.setVisible(false);
-        menuButton2.setVisible(false);
-        menuButton3.setVisible(false);
-        
-        leftMenu.setPrefWidth(30);
-        whole.setPrefWidth(stage.getWidth());
-        scrollPane.setPrefWidth(stage.getWidth()-32);
-        back.setPrefWidth(stage.getWidth()-32);
-        listV.setPrefWidth(back.getPrefWidth());
-        
-    }
+    private Pane animatedPanel;
     
+    ObservableList<Category> list;
+    List<PruebaController> listCont;
+    List<Category> listCat;
+    List<Charge> listChar;
+    
+    //Button adder position ratios
+    double bX, bY;
+    
+    //AnimatedPane
+    String name="", description="";
+    TextField nameField;
+    TextArea descriptionField;
+    void setControllerL(FXMLDocumentController controller)
+    {
+        this.controllerL = controller;
+    }
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        menuButton1.setVisible(false);
-        menuButton2.setVisible(false);
-        menuButton3.setVisible(false);
-
-        leftMenu.onMouseEnteredProperty().addListener((c)->{
-                    // TODO
-                    System.out.println("Hello");
-            menuButton1.setVisible(true);
-            menuButton2.setVisible(true);
-            menuButton3.setVisible(true);
-        });
-        leftMenu.onMouseClickedProperty().addListener((c)->{
-            System.out.println("Hello");
-            menuButton1.setVisible(true);
-            menuButton2.setVisible(true);
-            menuButton3.setVisible(true);
-        });
-        leftMenu.onMouseExitedProperty().addListener((c)->{
-                    // TODO
-            menuButton1.setVisible(false);
-            menuButton2.setVisible(false);
-            menuButton3.setVisible(false);
-        });
-    }    
-    public void trueInit()
-    {
-        this.stage = (Stage)leftMenu.getScene().getWindow();
-        menuButton1.setVisible(false);
-        menuButton2.setVisible(false);
-        menuButton3.setVisible(false);
-        listObjects = FXCollections.observableArrayList();
-        leftMenu.setPrefWidth(30);
-        whole.setPrefWidth(stage.getWidth());
-        scrollPane.setPrefWidth(stage.getWidth()-32);
-        back.setPrefWidth(stage.getWidth()-32);
-        listV.setPrefWidth(back.getPrefWidth());
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        adder.setOnAction(event -> showAnimatedPanel());
         try{
-            Node node = (Node) new FXMLLoader().load(getClass().getResource("prueba.fxml"));
-            back.getChildren().add(node);
-        }catch(IOException e){}
-        
-        
+            account = Acount.getInstance();
+            account.logInUserByCredentials("M","M");
+            List<Category> cats = account.getUserCategories();
+            for( int i = 0 ; i < cats.size() ; i++)
+            {
+                account.removeCategory(cats.get(i));
+            }
+            listCat = account.getUserCategories();
+            loadCreated();
+        }catch( AcountDAOException e){e.printStackTrace();}
+        catch(IOException e){e.printStackTrace();}
         
     }
+    void addButtons() throws IOException
+    {
+        bX = adder.getLayoutX()/(back.getWidth()+0.0);
+        System.out.println(bX);
+        bY = adder.getLayoutY()/(back.getHeight()+0.0);
+        System.out.println(bY);
+    }
+
     
-}
+    void adjustW(){
+        
+        if(animatedPanel != null)
+        {
+            animatedPanel.setVisible(false);
+            name =nameField.getText();
+            description = descriptionField.getText();
+        }
+        Scene scene = grid.getScene();
+        back.setPrefWidth(scene.getWidth());
+        scrollPane.setPrefWidth(scene.getWidth());
+        grid.setPrefWidth(scene.getWidth());
+        back.setMaxWidth(scene.getWidth());
+        scrollPane.setMaxWidth(scene.getWidth());
+        grid.setMaxWidth(scene.getWidth());
+        adder.setLayoutX((0.0+bX)*back.getWidth());
+    }
+    void adjustH(){
+        
+        if(animatedPanel != null)
+        {
+            animatedPanel.setVisible(false);
+            name =nameField.getText();
+            description = descriptionField.getText();
+            
+        }
+        Scene scene = grid.getScene();
+        back.setPrefHeight(scene.getHeight());
+        scrollPane.setPrefHeight(scene.getHeight());
+        grid.setPrefHeight(scene.getHeight());
+        back.setMaxHeight(scene.getHeight());
+        scrollPane.setMaxHeight(scene.getHeight());
+        grid.setMaxHeight(scene.getHeight());
+        adder.setLayoutY((bY+0.0)*back.getHeight()); 
+    }
+    
+    private void loadCreated() throws IOException
+    {
+        for (Category listCat1 : listCat) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("prueba.fxml"));
+            Node obj = loader.load();
+            PruebaController pController = loader.getController();
+            pController.setName(listCat1.getName());
+            pController.setPrice(listCat1.getDescription());
+            grid.add(obj, 0, 2+counterObj++, 3, 1);
+        }
+    }
+    private void showAnimatedPanel() {
+        if (animatedPanel != null) {
+            grid.getChildren().remove(animatedPanel);
+        }
+        animatedPanel = new Pane();
+        animatedPanel.setScaleY(0);
+        back.getChildren().add(animatedPanel);
+        animatedPanel.setStyle("-fx-background-color: lightblue;");
 
-/*listV.getItems().addAll("Item 1", "Item 2", "Item 3");
-        listV.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
-            @Override
-            public ListCell<String> call(ListView<String> listView) {
-                return new ListCell<String>() {
-                    @Override
-                    protected void updateItem(String item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty || item == null) {
-                            setText(null);
-                            setGraphic(null);
-                        } else {
-                            try {
-                                // Load the FXML file
-                                FXMLLoader loader = new FXMLLoader(getClass().getResource("prueba.fxml"));
-                                setGraphic(loader.load());
-
-                                // Get the controller and set the text
-                                PruebaController controller = loader.getController();
-                                controller.setItemText(item);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                                setText("Error loading item");
-                            }
-                        }
+        Text title = new Text("Create category");
+        nameField = new TextField();
+        descriptionField = new TextArea();
+        Button accept = new Button("Create");
+        accept.setOnAction((c)->{
+            try{
+                boolean added = account.registerCategory(nameField.getText(), descriptionField.getText());
+                if(added)
+                {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("prueba.fxml"));
+                    Node obj = loader.load();
+                    PruebaController pController = loader.getController();
+                    pController.setName(nameField.getText());
+                    pController.setPrice(descriptionField.getText());
+                    pController.setFatherController(controllerL, obj);
+                    if(2+counterObj <= 4)
+                    {
+                        grid.add(obj, 0, 2+counterObj++, 3, 1);
+                    }else{
+                        grid.add(obj, 0, 2+counterObj++, 3, 1);
+                        grid.setPrefHeight(grid.getHeight()+obj.maxHeight(bX) );
                     }
-                };
+                    pController.getBack().focusedProperty().addListener((d, oldV, newV)->{
+                        if(newV){ System.out.println("Hello");}
+                    });
+
+                    animatedPanel.setVisible(false);
+                }
+                
+            }catch(AcountDAOException e){}
+            catch(IOException b){}
+            
+            
+        });
+        nameField.setPromptText("Enter name");
+        descriptionField.setPromptText("Enter name");
+        animatedPanel.getChildren().add(nameField);
+        animatedPanel.getChildren().add(descriptionField);
+        animatedPanel.getChildren().add(accept);
+        animatedPanel.getChildren().add(title);
+        animatedPanel.setPrefWidth(back.getWidth()/3.0);
+        animatedPanel.setPrefHeight(back.getHeight()/3.0);
+        
+        animatedPanel.setMaxWidth(back.getWidth()/3.0);
+        animatedPanel.setMaxHeight(back.getHeight()/3.0);
+        animatedPanel.setLayoutX(adder.getLayoutX()+adder.getWidth()-animatedPanel.getPrefWidth());
+        animatedPanel.setLayoutY(adder.getLayoutY() + adder.getHeight()-animatedPanel.getPrefHeight());
+        
+        title.setLayoutY(20);
+        nameField.setLayoutX(10);
+        nameField.setLayoutY(30);
+        nameField.setPrefWidth(animatedPanel.getMaxWidth()-20);
+        descriptionField.setLayoutY(animatedPanel.getMaxHeight()*0.5-20);
+        descriptionField.setLayoutX(10);
+        descriptionField.setPrefWidth(animatedPanel.getMaxWidth()-20);
+        descriptionField.setPrefHeight(animatedPanel.getMaxHeight()*0.5-10);
+        nameField.setText(name);
+        descriptionField.setText(description);
+        accept.setLayoutY(animatedPanel.getMaxHeight()-25);
+        accept.setLayoutX((animatedPanel.getWidth()-accept.getWidth())/2);
+        
+        TranslateTransition translateTransition = new TranslateTransition(Duration.millis(250), animatedPanel);
+        translateTransition.setFromY(adder.getHeight());
+        translateTransition.setToY(0);
+
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(250), animatedPanel);
+        scaleTransition.setFromY(0);
+        scaleTransition.setToY(1);
+
+        translateTransition.play();
+        scaleTransition.play();
+        animatedPanel.requestFocus();
+        animatedPanel.focusedProperty().addListener((c, oldValue, newValue)->{
+            if(!newValue)
+            {
+                animatedPanel.setVisible(false);
+            }else{
+                animatedPanel.setVisible(true);
+                name =nameField.getText();
+                description = descriptionField.getText();
             }
-        });*/
+        }
+        );
+        descriptionField.focusedProperty().addListener((c, oldValue, newValue)->{
+            if(!newValue)
+            {
+                animatedPanel.setVisible(false);
+            }else{
+                animatedPanel.setVisible(true);
+                name =nameField.getText();
+                description = descriptionField.getText();
+            }
+        });
+        nameField.focusedProperty().addListener((c, oldValue, newValue)->{
+            if(!newValue)
+            {
+                animatedPanel.setVisible(false);
+            }else{
+                animatedPanel.setVisible(true);
+                name =nameField.getText();
+                description = descriptionField.getText();
+            }
+        }
+        );
+        accept.focusedProperty().addListener((c, oldValue, newValue)->{
+            if(!newValue)
+            {
+                animatedPanel.setVisible(false);
+            }else{
+                animatedPanel.setVisible(true);
+                name =nameField.getText();
+                description = descriptionField.getText();
+            }
+        });   
+    }
+    
+    void moveCat(Node node, MouseEvent event)
+    {
+        
+    }
+    void movingCat(Node node, MouseEvent event)
+    {
+        
+    }
+    void movedCat(Node node, MouseEvent event)
+    {
+        
+    }
+
+}
--- IPC_FXMLCore-master/src/javafxmlapplication/PruebaController.java
+++ IPC_FXMLCore-master/src/javafxmlapplication/PruebaController.java
@@ -1,18 +1,52 @@
 package javafxmlapplication;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.text.Text;
-public class PruebaController {
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+public class PruebaController{
     
     @FXML
     private Text name;
-
+    @FXML
+    private Button buttonMover;
     @FXML
     private Text price;
-
-    void setItemText(String text)
+    @FXML
+    private StackPane back;
+    private Node node;
+    
+    FXMLDocumentController fatherController;
+    void setFatherController(FXMLDocumentController fatherController, Node node){
+        this.fatherController = fatherController;
+        this.node = node;
+    }
+            
+    StackPane getBack()
+    {
+        return back;
+    }
+    @FXML
+    void move(MouseEvent event) {
+        fatherController.moveCat(node, event);
+    }
+    @FXML
+    void moving(MouseEvent event)
+    {
+        fatherController.movingCat(node, event);
+    }
+    @FXML
+    void moved(MouseEvent event) {
+        fatherController.movedCat(node, event);
+    }
+    void setName(String text)
     {
         this.name.setText(text);
     }
+    void setPrice(String price)
+    {
+        this.price.setText(price);
+    }
 }
-
