40f3a857a297ccec1588683449d60e67f86df14d
Aa049 <116162054+Aa049@users.noreply.github.com>
2024-05-26 16:03:56 +0200

    charge

---
+++ IPC_FXMLCore-master/src/javafxmlapplication/ChargePruebaController.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafxmlapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import model.Charge;
+
+public class ChargePruebaController {
+
+    @FXML
+    private Text name;
+    @FXML
+    private Button buttonMover;
+    @FXML
+    private Text price;
+    @FXML
+    private StackPane back;
+    @FXML
+    private Text date;
+    
+    private int pos;
+    private Node node;
+    FXMLDocumentController fatherController;
+    PruebaController pC;
+    //In order to move the node inside the screen
+    private double x, y, xL, yL;
+    double getX(){ return this.x; }
+    double getY(){ return this.y; }
+    void setX( double x ){ this.x = x; }
+    void setY( double y ){ this.y = y; }
+    double getXL(){ return this.xL; }
+    double getYL(){ return this.yL; }
+    void setXL( double x ){ this.xL = x; }
+    void setYL( double y ){ this.yL = y; }
+    String getName(){ return this.name.getText(); }
+    
+    
+    void setFatherController(FXMLDocumentController fatherController, Node node, PruebaController pC, int pos){
+        this.fatherController = fatherController;
+        this.node = node;
+        this.pC = pC;
+        this.pos = pos;
+    }
+            
+    StackPane getBack()
+    {
+        return back;
+    }
+    @FXML
+    void move(MouseEvent event) {
+        fatherController.moveCat(node, event, pC);
+    }
+    @FXML
+    void moving(MouseEvent event)
+    {
+        fatherController.movingCat(node, event, pC);
+    }
+    @FXML
+    void moved(MouseEvent event) {
+        fatherController.movedCat(node, event, pC);
+    }
+    void setName(String text)
+    {
+        this.name.setText(text);
+    }
+    void setPrice(String price)
+    {
+        this.price.setText(price);
+    }
+    void setDate(String date){
+        this.date.setText(date);
+    }
+}
